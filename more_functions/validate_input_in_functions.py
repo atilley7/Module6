13369b0f5c21799616886400b98fdde2d1bf0cd3
atilley7 <averytilley7@gmail.com>
2020-10-06 00:03:20 -0500

    test_score_input_ test_score_below_range and test_score_input_ test_score_above_range pass

--- more_functions/validate_input_in_functions.py
+++ more_functions/validate_input_in_functions.py
@@ -10,4 +10,5 @@ def score_input(test_name, test_score=0, invalid_message='Invalid test score, tr
     # return {test_name:test_score}
     if 0 <= test_score <= 100:
         return str(test_name) + ': ' + str(test_score)
-
+    else:
+        return invalid_message
