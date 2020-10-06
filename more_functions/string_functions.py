77cb2c0b415dea187da2048e1d6ae538534aebd8
atilley7 <averytilley7@gmail.com>
2020-10-05 22:50:33 -0500

    added docstring

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,2 +1,9 @@
+""" multiplies a string a given number of times
+    :param strOne, string to be multiplied
+    :param num, times to mulitply strOne
+    :returns string entered by number of times desired
+    """
+
+
 def multiply_string(strOne, num):
     return strOne * num
