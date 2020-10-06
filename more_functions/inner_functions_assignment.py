d61aab67fabf034886b0c0592bd34a68ee09fca9
atilley7 <averytilley7@gmail.com>
2020-10-06 00:38:14 -0500

    fixed for square

--- more_functions/inner_functions_assignment.py
+++ more_functions/inner_functions_assignment.py
@@ -1,8 +1,14 @@
 def measurements(list):
     def area(a_list):
-        return a_list[0] * a_list[1]
+        if len(a_list) == 2:
+            return a_list[0] * a_list[1]
+        else:
+            return a_list[0] * a_list[0]
 
     def perimeter(a_list):
-        return a_list[0] * 2 + a_list[1] * 2
+        if len(a_list) == 2:
+            return a_list[0] * 2 + a_list[1] * 2
+        else:
+            return a_list[0]*4
 
     return 'Perimeter = ' + str(perimeter(list)) + ' Area = ' + str(area(list))
